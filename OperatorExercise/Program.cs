5a489968c36b8065b27e64b9e026a551d81f8a0d
jshudd <jshudd@gmail.com>
2020-04-03 10:51:14 -0500

    initial code

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var sum = a + b;
+            Console.WriteLine(sum);
+
+            var diff = a - b;
+            Console.WriteLine(diff);
+
+            var mult = a * b;
+            Console.WriteLine(mult);
+
+            var quotient = a / b;
+            Console.WriteLine(quotient);
+
+            var remainder = a % b;
+            Console.WriteLine(remainder);
+        }
+    }
+}
